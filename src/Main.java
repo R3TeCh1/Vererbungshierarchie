6bd6b9d937446e85afc868e84b16d346734113b8
David Rischow <drischow@stud.hs-heilbronn.de>
2023-10-15 21:06:28 +0200

    Create basic version of main class

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Mitarbeiter kadir = new Angestellter();
+        Mitarbeiter david = new ATAngestellter("David", "Rischow", 10000);
+        Mitarbeiter nazanin = new Zeitarbeiter();
+
+    }
+}
