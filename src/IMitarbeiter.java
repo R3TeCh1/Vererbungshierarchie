49c183426296f618203ef637cfd2930d6d2dcc59
David Rischow <drischow@stud.hs-heilbronn.de>
2023-10-22 23:48:13 +0200

    Create IMitarbeiter and ISteuerZahler

---
+++ src/IMitarbeiter.java
@@ -0,0 +1,5 @@
+public interface IMitarbeiter {
+    final float mindestLohn = 12; // Unveränderliche Variable (Konstante)
+    
+    float entgeltBerechnen();
+}
---
+++ src/ISteuerZahler.java
@@ -0,0 +1,4 @@
+public interface ISteuerZahler {
+    float tatsächlicheEinkommenSteuer();
+    float voraussichtlicheEinkommenSteuer();
+}
